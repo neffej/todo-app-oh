4d048eafa4c0dae7dfd80c9ba8c60358b4645c7c
neffej <118024182+neffej@users.noreply.github.com>
2023-02-08 18:15:54 -0500

    push server files

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("am I running?");
