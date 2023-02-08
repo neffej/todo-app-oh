b11b852e9718f661edab1fa4cfeb80b35c6a4551
neffej <118024182+neffej@users.noreply.github.com>
2023-02-08 18:54:57 -0500

    added views and handlebars

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+const exphbs = require('express-handlebars')
 const express = require('express');
 const routes = require("./controllers");
 const sequelize = require('./config/connection');
 // Keep the item below in mind - if you don't CALL the models folder to just generate thge tables, they will never be made.
-const models = require('./models');
+// const models = require('./models');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// This makes the express application use the route content
 app.use(express.static("public"));
 app.use(routes);
 
